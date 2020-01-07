64f94403c8f8fbde0eb68b323bc257365a4c748b
unknown <camlump@yahoo.com>
2020-01-06 23:41:49 -0500

    created game logic

---
+++ Memory_game/js/main.js
@@ -0,0 +1,11 @@
+
+
+let cardOne = "king";
+let cardTwo = "queen";
+let cardThree = "king";
+let cardFour = "queen";
+
+
+console.log("User flipped " + cardFour);
+console.log("User flipped " + cardThree);
+
