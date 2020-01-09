f66de49af23be9bcfd81fae85f8ca1f8ccf790f0
unknown <camlump@yahoo.com>
2020-01-09 18:23:28 -0500

    created game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -1,11 +1,44 @@
 
 
-let cardOne = "king";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "queen";
 
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardThree);
 
+let cards = ["queen", "queen", "king", "king",];
+
+let cardOne = cards[0];
+
+let cardsInPlay = [];
+
+cardsInPlay.push('cardOne');
+/*
+console.log("User flipped " + cardOne);
+*/
+
+let cardTwo = cards[2];
+cardsInPlay.push('cardTwo');
+
+
+
+
+if (cardsInPlay.length === 2) {
+	alert("You found a match");
+} 
+
+else if(cardsInPlay[0] != cardsInPlay[2]); {
+	alert("Sorry try again"); 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("User flipped" + cardOne);
+console.log("User flipped " + cardTwo);
