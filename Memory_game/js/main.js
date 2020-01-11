0f2f7db70b7b5d4c4cf097f0d895929d44be35ff
unknown <camlump@yahoo.com>
2020-01-11 13:54:28 -0500

    created game logic

--- Memory_game/js/main.js
+++ Memory_game/js/main.js
@@ -4,9 +4,51 @@ let cardTwo = "queen";
 let cardThree = "king";
 let cardFour = "king";
 
-let cards = ["queen", "queen", "king", "king"];
 
-let cardsInPlay = [];
+
+let cards = [
+{
+	rank: "queen",
+
+	suit: "hearts",
+
+	cardImage: 'images/queen-of-hearts.png'
+
+},
+
+{
+	rank: "queen",
+
+	suit: "diamonds",
+
+	cardImage: "images/queen-of-diamonds"
+
+},
+
+{
+	rank: "king",
+
+	suit: "hearts",
+
+	cardImage: "images/king-of-hearts.png"
+
+},
+
+{
+	rank: "king",
+
+	suit: "diamonds",
+
+	cardImage: 'images/king-of-diamonds.png'
+
+}
+
+];
+
+
+const cardsInPlay = [];
+
+
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -14,22 +56,33 @@ function checkForMatch() {
 } else {
   console.log("Sorry, try again.");
 	}
+
+
+
 }
 
 
 
 
 
-function flipCard(cardId){
+function flipCard(cardId) {
 
-cardsInPlay.push(cards[cardId]);
-	checkForMatch();
-console.log("User Flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId].rank);
+cardsInPlay.push(cards[cardId].suit);
+	
 
-};
+console.log("User Flipped " + cards[cardId].rank);
+checkForMatch();
+
+
+console.log(cards[cardId].cardImage);
+console.log(cardsInPlay[cardId].suit);
+
+}
 
 flipCard(0);
 flipCard(2);
+
 
 
 
